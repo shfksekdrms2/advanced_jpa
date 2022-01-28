c0a106ab7d690f72addcebf2b75a0d1456abcbf3
jsj0828 <jsj0828@midasin.com>
2022-01-28 15:43:32 +0900

    inflearn-43 [API 개발 고급 - 컬렉션 조회 최적화] 주문 조회 V6: JPA에서 DTO로 직접 조회, 플랫 데이터 최적화 - 주문 조회 V6: JPA에서 DTO로 직접 조회, 플랫 데이터 최적화

---
+++ src/main/java/jpabook/jpashop/repository/order/query/OrderFlatDto.java
@@ -0,0 +1,44 @@
+package jpabook.jpashop.repository.order.query;
+
+import jpabook.jpashop.domain.Address;
+import jpabook.jpashop.domain.OrderStatus;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Setter
+@Getter
+public class OrderFlatDto {
+
+    private Long orderId;
+    private String name;
+    private LocalDateTime orderDate;
+    private OrderStatus orderStatus;
+    private Address address;
+//    private List<OrderItemQueryDto> orderItems;
+
+    private String itemName;
+    private int orderPrice;
+    private int count;
+
+
+    public OrderFlatDto(Long orderId,
+                        String name,
+                        LocalDateTime orderDate,
+                        OrderStatus orderStatus,
+                        Address address,
+                        String itemName,
+                        int orderPrice,
+                        int count) {
+        this.orderId = orderId;
+        this.name = name;
+        this.orderDate = orderDate;
+        this.orderStatus = orderStatus;
+        this.address = address;
+//        this.orderItems = orderItems;
+        this.itemName = itemName;
+        this.orderPrice = orderPrice;
+        this.count = count;
+    }
+}
